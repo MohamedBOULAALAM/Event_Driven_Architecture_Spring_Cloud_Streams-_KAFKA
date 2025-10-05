0fc3589f463119c4d2efe2f55cfc41bebff36822
MohamedBOULAALAM <mohamedboulaalam01@gmail.com>
2025-10-05 23:54:18 +0100

    initialisation de projet

---
+++ src/main/java/b/m29/eda_kafka/EventDrivenArchitectureKafkaApplication.java
@@ -0,0 +1,13 @@
+package b.m29.eda_kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventDrivenArchitectureKafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventDrivenArchitectureKafkaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/b/m29/eda_kafka/EventDrivenArchitectureKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package b.m29.eda_kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventDrivenArchitectureKafkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
